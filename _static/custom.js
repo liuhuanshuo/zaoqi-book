67a8650c1158f75149a7f55b0959578465e57bb8
刘早起 <389159297@qq.com>
2021-12-13 18:16:39 +0800

    Update documentation

--- _static/custom.js
+++ _static/custom.js
@@ -39,6 +39,7 @@ document.addEventListener("DOMContentLoaded", function() {
     $('.btn__top').on('click', function(event) { event.preventDefault();
         event.stopPropagation();
         $('html, body').animate({ scrollTop: 0 }, 'slow'); });
+
     $('.btn__fullscreen').on('click', function() { event.preventDefault();
         event.stopPropagation();
         $(this).toggleClass('btn-active'); if (document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement || document.msFullscreenElement) { if (document.exitFullscreen) { document.exitFullscreen(); } else if (document.msExitFullscreen) { document.msExitFullscreen(); } else if (document.mozCancelFullScreen) { document.mozCancelFullScreen(); } else if (document.webkitExitFullscreen) { document.webkitExitFullscreen(); } } else { if (document.documentElement.requestFullscreen) { document.documentElement.requestFullscreen(); } else if (document.documentElement.webkitRequestFullscreen) { document.documentElement.webkitRequestFullscreen(); } else if (document.documentElement.mozRequestFullScreen) { document.documentElement.mozRequestFullScreen(); } else if (document.documentElement.msRequestFullscreen) { document.documentElement.msRequestFullscreen(); } } });
